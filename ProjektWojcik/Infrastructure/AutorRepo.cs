4a6fef34e232e85cb8daf4111037ecfe4993aa16
Student <Student@IISI-510-11>
2018-10-24 10:16:58 +0200

    Autor Repo v1.

---
+++ ProjektWojcik/Infrastructure/AutorRepo.cs
@@ -0,0 +1,39 @@
+﻿using ProjektWojcik.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektWojcik.Infrastructure
+{
+    public class AutorRepo
+    {
+        KsiegarniaKontekst _db;
+
+        public AutorRepo(KsiegarniaKontekst db) {
+            _db = db;
+        }
+
+        public IEnumerable<Autor> GetAll() {
+            return _db.Autorzy.ToList();
+        }
+        public Autor FindById(int id) {
+            return _db.Autorzy.SingleOrDefault(x => x.Id == id);   
+        }
+
+        public void Insert(Autor autor) {
+            _db.Autorzy.Add(autor);
+            _db.SaveChanges();
+        }
+
+        public void Update(Autor autor) {
+
+        }
+
+        public void Delete(int id) {
+            var autor = _db.Autorzy.SingleOrDefault(x => x.Id == id);
+            _db.Autorzy.Remove(autor);
+            _db.SaveChanges();
+        }
+    }
+}
